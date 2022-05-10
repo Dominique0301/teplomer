4c474835595f3a5f58ff4394bff13dd58cac10b4
Dominik Slehofer <93545274+Dominique0301@users.noreply.github.com>
2022-05-10 15:13:56 +0200

    Add files via upload

--- teplomer.c
+++ teplomer.c
@@ -1,15 +1,57 @@
+#include "spse_stm8.h"
 #include "stm8s.h"
 #include "milis.h"
-#include "stm8_hd44780.h"
-#include "spse_stm8.h"
-#include "delay.h"
-#include <stdio.h>
-#define _ISOC99_SOURCE
-#define _GNU_SOURCE
+#include "stm8s_gpio.h"
+#include "stdio.h"
+
+
+void ADC_init(void);
+
+#define STAV_START 1 
+#define STAV_STOP 2 
+#define STAV_ZAVORA 3
+
+uint32_t prevod=0,vysledek=0,y=0;
+uint16_t x=0,z=0;
+uint16_t last_time=0,time=0;
+
+
+void main(void){
+CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
+
+GPIO_Init(GPIOD,GPIO_PIN_4,GPIO_MODE_OUT_PP_LOW_SLOW);
+GPIO_Init(GPIOC,GPIO_PIN_5,GPIO_MODE_OUT_PP_LOW_SLOW);
+init_milis(); 
+ADC_init();
+init_uart1();
+
+  while (1){
+
+    if(milis()-last_time == 500 && x==0){
+      GPIO_WriteHigh(GPIOC, GPIO_PIN_5);
+      last_time=milis();
+      x=1;
+    }
+    
+    if(milis()-last_time == 500 && x==1){
+      GPIO_WriteLow(GPIOC, GPIO_PIN_5);
+      last_time=milis();
+      x=0;
+      y= ADC_get(ADC2_CHANNEL_2); 
+      vysledek= 5000*y/1024;
+     printf("U= %ldmV\n\r",vysledek);
+
+    }
+   
+  }
+}
+
+
 
 void ADC_init(void){
 // na pinech/vstupech ADC_IN2 (PB2) a ADC_IN3 (PB3) vypneme vstupní buffer
 ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL2,DISABLE);
+ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL3,DISABLE);
 // nastavíme clock pro ADC (16MHz / 4 = 4MHz)
 ADC2_PrescalerConfig(ADC2_PRESSEL_FCPU_D4);
 // volíme zarovnání výsledku (typicky vpravo, jen vyjmečně je výhodné vlevo)
@@ -20,50 +62,6 @@ ADC2_Select_Channel(ADC2_CHANNEL_2);
 ADC2_Cmd(ENABLE);
 // počkáme než se AD převodník rozběhne (~7us)
 ADC2_Startup_Wait();
-}
-void TIM2_setup(void){
-    TIM2_DeInit();
-    TIM2_TimeBaseInit(TIM2_PRESCALER_1, 640 - 1);//25kHz    
-    TIM2_OC1Init(TIM2_OCMODE_PWM1, TIM2_OUTPUTSTATE_ENABLE, 200, 
-                TIM2_OCPOLARITY_LOW);
-    TIM2_Cmd(ENABLE);
-
-}
-
-
-void init(void)
-{
-    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
-    init_milis();
-    lcd_init();
-    ADC_init();
-    TIM2_setup();
-}
-
-
-int main(void)
-{
-    init();
-    uint32_t timeA = 0;
-    uint16_t adc_value;
-    char text[32];
-    int16_t teplota;
-    int16_t teplota_1_cast;
-    int16_t teplota_2_cast;
-
-    while (1) {
-        if (milis()-timeA>100){
-            timeA=milis();
-            adc_value = ADC_get(ADC2_CHANNEL_2); // do adc_value ulož výsledek převodu vstupu ADC_IN2 (PB2)
-            teplota = ((uint32_t)adc_value*5000 + 512)/1024;
-            teplota_1_cast=teplota/10;//celočíselná část
-            teplota_2_cast=teplota%10;//desetinná část
-
-        }
-        lcd_gotoxy(0, 0);
-        sprintf(text,"Teplota = %2u %1u C",teplota_1_cast,teplota_2_cast);
-        lcd_puts(text);
-    }
 }
 
 /*-------------------------------  Assert -----------------------------------*/
